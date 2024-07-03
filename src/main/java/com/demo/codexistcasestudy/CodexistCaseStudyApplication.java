84d85b68e2b7b7c1b09a003b3d64f5f9e072220b
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-07-03 19:49:47 +0300

    Added Dependencies

---
+++ src/main/java/com/demo/codexistcasestudy/CodexistCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.demo.codexistcasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexistCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodexistCaseStudyApplication.class, args);
+	}
+
+}
