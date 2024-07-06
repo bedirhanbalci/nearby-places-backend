adbc7a4ad1cc7da55e4ae63f3b408bb00887f8c3
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-07-07 01:10:15 +0300

    Added OpenAPI docs for Place API GET endpoint

--- src/main/java/com/demo/codexistcasestudy/controller/MainPlaceController.java
+++ src/main/java/com/demo/codexistcasestudy/controller/MainPlaceController.java
@@ -3,6 +3,12 @@ package com.demo.codexistcasestudy.controller;
 import com.demo.codexistcasestudy.dto.PlaceResponseDto;
 import com.demo.codexistcasestudy.service.MainPlaceService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Google Map API V1", description = "Google Map API to filter current latitude, longitude, radius by nearby places")
 public class MainPlaceController {
 
     private final MainPlaceService mainPlaceService;
@@ -19,6 +26,28 @@ public class MainPlaceController {
         this.mainPlaceService = mainPlaceService;
     }
 
+    @Operation(
+            method = "GET",
+            summary = "Google Map API V1",
+            description = "Google Map API to filter current latitude, longitude, radius by nearby places",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Success",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = PlaceResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Bad Request",
+                            content = @Content(
+                                    schema = @Schema(hidden = true)
+                            )
+                    )
+            }
+    )
     @GetMapping("/place")
     public ResponseEntity<PlaceResponseDto> getNearbyPlaces(@RequestParam Double longitude, @RequestParam Double latitude, @RequestParam Double radius) throws JsonProcessingException {
 
